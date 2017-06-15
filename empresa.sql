cc375b770eb6c6de1ebcd289b8958e5c060c7226
Faria Alencar <Faria Alencar>
2017-06-14 23:49:06 -0300

    fix: Update to export output

--- empresa.sql
+++ empresa.sql
@@ -1,79 +1,84 @@
-DROP TABLE IF EXISTS empregado;
-CREATE TABLE IF NOT EXISTS empregado (
-	id_empregado INTEGER PRIMARY KEY  AUTOINCREMENT,
-	primeiro_nome TEXT NOT NULL,
-	ultimo_nome TEXT NOT NULL,
-	endereco TEXT NOT NULL,
-	sexo TEXT,
-	salario REAL
-);
-INSERT INTO empregado values (null,'Alice','Silva','Rua José Antonio, 156','m',3555);
-INSERT INTO empregado values (null,'Marcos','Mendes','Rua Salmão, 100','m',3000.3);
-INSERT INTO empregado values (null,'Sofia','Wong','Avenida Rio Branco, 100','f',3500);
-INSERT INTO empregado values (null,'James','Nunes','Rua Salmão, 20','m',4000);
-INSERT INTO empregado values (null,'Vitória','Smith','Rua Adhemar de Barros, 3000','f',2000);
-INSERT INTO empregado values (null,'Pedro','Santos','Rua Ilha do Sul, 350','m',2000.5);
+﻿--
+-- File generated with SQLiteStudio v3.1.1 on Wed Jun 14 23:46:37 2017
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
 
-
-
+-- Table: dependente
 DROP TABLE IF EXISTS dependente;
-CREATE TABLE IF NOT EXISTS dependente (
-	id_dependente INTEGER PRIMARY KEY  AUTOINCREMENT,
-	nome TEXT NOT NULL,
-	sexo TEXT NOT NULL,
-	parentesco TEXT NOT NULL,
-	id_empregado INTEGER,
-	
-	FOREIGN KEY (id_empregado) REFERENCES empregado(id_empregado)
+
+CREATE TABLE dependente (
+    id_dependente INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome          TEXT    NOT NULL,
+    sexo          TEXT    NOT NULL,
+    parentesco    TEXT    NOT NULL,
+    id_empregado  INTEGER,
+    FOREIGN KEY (
+        id_empregado
+    )
+    REFERENCES empregado (id_empregado) 
 );
-INSERT INTO dependente VALUES (null,'Carlos','m','Filho',1);
-INSERT INTO dependente VALUES (null,'Daniel','m','Filho',1);
-INSERT INTO dependente VALUES (null,'Elizabeth','f','Esposa',2);
-INSERT INTO dependente VALUES (null,'Miguel','m','Filho',6);
-INSERT INTO dependente VALUES (null,'Charlie','m','Marido',6);
 
+INSERT INTO dependente (id_dependente, nome, sexo, parentesco, id_empregado) VALUES (1, 'Carlos', 'm', 'Filho', 1);
+INSERT INTO dependente (id_dependente, nome, sexo, parentesco, id_empregado) VALUES (2, 'Daniel', 'm', 'Filho', 1);
+INSERT INTO dependente (id_dependente, nome, sexo, parentesco, id_empregado) VALUES (3, 'Elizabeth', 'f', 'Esposa', 2);
+INSERT INTO dependente (id_dependente, nome, sexo, parentesco, id_empregado) VALUES (4, 'Miguel', 'm', 'Filho', 6);
+INSERT INTO dependente (id_dependente, nome, sexo, parentesco, id_empregado) VALUES (5, 'Charlie', 'm', 'Marido', 6);
 
+-- Table: empregado
+DROP TABLE IF EXISTS empregado;
+
+CREATE TABLE empregado (
+    id_empregado  INTEGER PRIMARY KEY AUTOINCREMENT,
+    primeiro_nome TEXT    NOT NULL,
+    ultimo_nome   TEXT    NOT NULL,
+    endereco      TEXT    NOT NULL,
+    sexo          TEXT,
+    salario       REAL
+);
+
+INSERT INTO empregado (id_empregado, primeiro_nome, ultimo_nome, endereco, sexo, salario) VALUES (1, 'Alice', 'Silva', 'Rua José Antonio, 156', 'm', 3555);
+INSERT INTO empregado (id_empregado, primeiro_nome, ultimo_nome, endereco, sexo, salario) VALUES (2, 'Marcos', 'Mendes', 'Rua Salmão, 100', 'm', 3000.3);
+INSERT INTO empregado (id_empregado, primeiro_nome, ultimo_nome, endereco, sexo, salario) VALUES (3, 'Sofia', 'Wong', 'Avenida Rio Branco, 100', 'f', 3500);
+INSERT INTO empregado (id_empregado, primeiro_nome, ultimo_nome, endereco, sexo, salario) VALUES (4, 'James', 'Nunes', 'Rua Salmão, 20', 'm', 4000);
+INSERT INTO empregado (id_empregado, primeiro_nome, ultimo_nome, endereco, sexo, salario) VALUES (5, 'Vitória', 'Smith', 'Rua Adhemar de Barros, 3000', 'f', 2000);
+INSERT INTO empregado (id_empregado, primeiro_nome, ultimo_nome, endereco, sexo, salario) VALUES (6, 'Pedro', 'Santos', 'Rua Ilha do Sul, 350', 'm', 2000.5);
+
+-- Table: projeto
 DROP TABLE IF EXISTS projeto;
-CREATE TABLE IF NOT EXISTS projeto (
-	cod_proj INTEGER PRIMARY KEY NOT NULL,
-	nome TEXT NOT NULL	
+
+CREATE TABLE projeto (
+    cod_proj INTEGER PRIMARY KEY
+                     NOT NULL,
+    nome     TEXT    NOT NULL
 );
-INSERT INTO projeto VALUES (100,'Projeto 1000');
-INSERT INTO projeto VALUES (200,'Projeto 2000');
-INSERT INTO projeto VALUES (300,'Projeto 3000');
-INSERT INTO projeto VALUES (400,'Projeto 4000');
 
+INSERT INTO projeto (cod_proj, nome) VALUES (100, 'Projeto 1000');
+INSERT INTO projeto (cod_proj, nome) VALUES (200, 'Projeto 2000');
+INSERT INTO projeto (cod_proj, nome) VALUES (300, 'Projeto 3000');
+INSERT INTO projeto (cod_proj, nome) VALUES (400, 'Projeto 4000');
 
+-- Table: trabalha
 DROP TABLE IF EXISTS trabalha;
-CREATE TABLE IF NOT EXISTS trabalha (
-	id_trabalha INTEGER PRIMARY KEY  AUTOINCREMENT,
-	qtd_horas INTEGER NOT NULL,
-	funcao TEXT NOT NULL,
-	cod_proj INTEGER NOT NULL,
-	id_empregado INTEGER NOT NULL,
-	
-	FOREIGN KEY (id_empregado) REFERENCES empregado(id_empregado)
-		
+
+CREATE TABLE trabalha (
+    id_trabalha  INTEGER PRIMARY KEY AUTOINCREMENT,
+    qtd_horas    INTEGER NOT NULL,
+    funcao       TEXT    NOT NULL,
+    cod_proj     INTEGER NOT NULL,
+    id_empregado INTEGER NOT NULL,
+    FOREIGN KEY (
+        id_empregado
+    )
+    REFERENCES empregado (id_empregado) 
 );
-INSERT INTO trabalha VALUES (null,10,'programador',100,1);
-INSERT INTO trabalha VALUES (null,5,'analista',100,2);
-INSERT INTO trabalha VALUES (null,5,'testador',300,3);
-INSERT INTO trabalha VALUES (null,10,'analista',400,3);
-INSERT INTO trabalha VALUES (null,5,'analista',200,4);
-INSERT INTO trabalha VALUES (null,3,'projetista',300,4);
-INSERT INTO trabalha VALUES (null,7,'testador',200,6);
 
---EXE 2, aula 2
-SELECT primeiro_nome,sexo,salario*1.2 from empregado;
-SELECT nome FROM dependente WHERE parentesco='Filho' OR parentesco='filho';
-SELECT primeiro_nome,ultimo_nome,salario from empregado WHERE salario BETWEEN 2000 AND 4000;
-SELECT funcao FROM empregado e 
-	INNER JOIN trabalha t ON e.id_empregado=t.id_empregado
-	WHERE t.qtd_horas > 5;
-SELECT primeiro_nome,salario FROM empregado WHERE primeiro_nome LIKE "%a%";
-SELECT * FROM empregado WHERE endereco LIKE "%r%" OR endereco LIKE "%R%";
-SELECT nome,parentesco FROM dependente WHERE id_empregado=1;
-SELECT * from empregado WHERE sexo='f' AND (primeiro_nome LIKE "%i%" OR ultimo_nome LIKE "%i");
+INSERT INTO trabalha (id_trabalha, qtd_horas, funcao, cod_proj, id_empregado) VALUES (1, 10, 'programador', 100, 1);
+INSERT INTO trabalha (id_trabalha, qtd_horas, funcao, cod_proj, id_empregado) VALUES (2, 5, 'analista', 100, 1);
+INSERT INTO trabalha (id_trabalha, qtd_horas, funcao, cod_proj, id_empregado) VALUES (3, 5, 'testador', 300, 1);
+INSERT INTO trabalha (id_trabalha, qtd_horas, funcao, cod_proj, id_empregado) VALUES (4, 10, 'analista', 400, 1);
 
-
-
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
--- pedidos.sql
+++ pedidos.sql
@@ -1,11 +1,22 @@
+--
+-- File generated with SQLiteStudio v3.1.1 on Wed Jun 14 23:48:00 2017
+--
+-- Text encoding used: System
+--
+PRAGMA foreign_keys = off;
+BEGIN TRANSACTION;
+
+-- Table: cliente
 DROP TABLE IF EXISTS cliente;
+
 CREATE TABLE cliente (
-    id_cliente INTEGER PRIMARY KEY AUTOINCREMENT, 
-    nome VARCHAR (100) NOT NULL, 
-    cpf VARCHAR (10) NOT NULL, 
-    telefone VARCHAR (9) NOT NULL,
-    endereco VARCHAR (200) NOT NULL
-    );    
+    id_cliente INTEGER       PRIMARY KEY AUTOINCREMENT,
+    nome       VARCHAR (100) NOT NULL,
+    cpf        VARCHAR (10)  NOT NULL,
+    telefone   VARCHAR (9)   NOT NULL,
+    endereco   VARCHAR (200) NOT NULL
+);
+
 INSERT INTO cliente (id_cliente, nome, cpf, telefone, endereco) VALUES (1, 'Carlos', '7777', '1234-1234', 'Rua jose cobra, 101');
 INSERT INTO cliente (id_cliente, nome, cpf, telefone, endereco) VALUES (2, 'Clarissa', '8888', '5555-3434', 'Avenida Ademar de Barros , 302');
 INSERT INTO cliente (id_cliente, nome, cpf, telefone, endereco) VALUES (3, 'Elisa', '414141', '3232-1212', 'Rua das Flores, 201');
@@ -13,50 +24,55 @@ INSERT INTO cliente (id_cliente, nome, cpf, telefone, endereco) VALUES (4, 'Mari
 INSERT INTO cliente (id_cliente, nome, cpf, telefone, endereco) VALUES (5, 'Ugo', '3490', '2323-1256', 'Avenida Onze de Agosto, 23');
 INSERT INTO cliente (id_cliente, nome, cpf, telefone, endereco) VALUES (6, 'Olavo', '5990', '1111-4444', 'Rua Jose Bissoto, 22');
 
--- Table: Produto
-DROP TABLE IF EXISTS Produto;
-CREATE TABLE Produto (
-    id_produto INTEGER PRIMARY KEY AUTOINCREMENT, 
-    descricao VARCHAR (100) NOT NULL, 
-    preco REAL NOT NULL
-    );
-INSERT INTO Produto (id_produto, descricao, preco) VALUES (50, 'caneta', '1.3');
-INSERT INTO Produto (id_produto, descricao, preco) VALUES (51, 'Lapis', '1.2');
-INSERT INTO Produto (id_produto, descricao, preco) VALUES (52, 'Caderno', '5.5');
-INSERT INTO Produto (id_produto, descricao, preco) VALUES (53, 'Borracha', '0.7');
-INSERT INTO Produto (id_produto, descricao, preco) VALUES (54, 'Apagador', '3.35');
+-- Table: ItemPedido
+DROP TABLE IF EXISTS ItemPedido;
 
+CREATE TABLE ItemPedido (
+    id_item_pedido  INTEGER         PRIMARY KEY AUTOINCREMENT,
+    quantidade      INTEGER (4)     NOT NULL,
+    valor_sub_total INTEGER (10, 2) NOT NULL,
+    id_pedido_flc   INTEGER (10)    REFERENCES Pedido (id_pedido),
+    id_produto_flc  INTEGER (10)    REFERENCES Produto (id_produto) 
+);
+
+INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (30, 5, 6.5, 10, 50);
+INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (31, 1, 5.5, 11, 52);
+INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (32, 5, 3.5, 11, 53);
+INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (33, 3, 16.5, 12, 52);
+INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (100, 5, 27.5, 1, 52);
+INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (101, 2, 1.4, 1, 53);
+INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (150, 10, 33.5, 15, 54);
 
 -- Table: Pedido
 DROP TABLE IF EXISTS Pedido;
+
 CREATE TABLE Pedido (
-    id_pedido INTEGER PRIMARY KEY AUTOINCREMENT, 
-    valor_total INTEGER (10, 2) NOT NULL, 
-    data TEXT NOT NULL, 
-    id_cliente_flc INTEGER REFERENCES cliente (id_cliente)
-    );
-INSERT INTO Pedido (id_pedido, valor_total, data, id_cliente_flc) VALUES (10, '6.5', '10/10/2014', 1);
+    id_pedido      INTEGER         PRIMARY KEY AUTOINCREMENT,
+    valor_total    INTEGER (10, 2) NOT NULL,
+    data           TEXT            NOT NULL,
+    id_cliente_flc INTEGER         REFERENCES cliente (id_cliente) 
+);
+
+INSERT INTO Pedido (id_pedido, valor_total, data, id_cliente_flc) VALUES (1, 28.9, '12/01/2015', 4);
+INSERT INTO Pedido (id_pedido, valor_total, data, id_cliente_flc) VALUES (10, 6.5, '10/10/2014', 1);
 INSERT INTO Pedido (id_pedido, valor_total, data, id_cliente_flc) VALUES (11, 9, '01/10/2014', 3);
-INSERT INTO Pedido (id_pedido, valor_total, data, id_cliente_flc) VALUES (12, '16.5', '20/10/2014', 5);
-INSERT INTO Pedido (id_pedido, valor_total, data, id_cliente_flc) VALUES (15, '33.5', '19/02/2016', 6);
-INSERT INTO Pedido (id_pedido, valor_total, data, id_cliente_flc) VALUES (1, '28.9', '12/01/2015', 4);
+INSERT INTO Pedido (id_pedido, valor_total, data, id_cliente_flc) VALUES (12, 16.5, '20/10/2014', 5);
+INSERT INTO Pedido (id_pedido, valor_total, data, id_cliente_flc) VALUES (15, 33.5, '19/02/2016', 66);
 
+-- Table: Produto
+DROP TABLE IF EXISTS Produto;
 
+CREATE TABLE Produto (
+    id_produto INTEGER       PRIMARY KEY AUTOINCREMENT,
+    descricao  VARCHAR (100) NOT NULL,
+    preco      REAL          NOT NULL
+);
 
--- Table: ItemPedido
-DROP TABLE IF EXISTS ItemPedido;
-CREATE TABLE ItemPedido (
-    id_item_pedido INTEGER PRIMARY KEY AUTOINCREMENT, 
-    quantidade INTEGER (4) NOT NULL, 
-    valor_sub_total INTEGER (10, 2) NOT NULL, 
-    id_pedido_flc INTEGER (10) REFERENCES Pedido (id_pedido), 
-    id_produto_flc INTEGER (10) REFERENCES Produto (id_produto)
-    );
-INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (30, 5, '6.5', 10, 50);
-INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (31, 1, '5.5', 11, 52);
-INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (32, 5, '3.5', 11, 53);
-INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (33, 3, '16.5', 12, 52);
-INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (100, 5, '27.5', 1, 52);
-INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (101, 2, '1.4', 1, 53);
-INSERT INTO ItemPedido (id_item_pedido, quantidade, valor_sub_total, id_pedido_flc, id_produto_flc) VALUES (150, 10, '33.5', 15, 54);
+INSERT INTO Produto (id_produto, descricao, preco) VALUES (50, 'caneta', 1.3);
+INSERT INTO Produto (id_produto, descricao, preco) VALUES (51, 'Lapis', 1.2);
+INSERT INTO Produto (id_produto, descricao, preco) VALUES (52, 'Caderno', 5.5);
+INSERT INTO Produto (id_produto, descricao, preco) VALUES (53, 'Brracha', 0.7);
+INSERT INTO Produto (id_produto, descricao, preco) VALUES (54, 'Apagador', 3.35);
 
+COMMIT TRANSACTION;
+PRAGMA foreign_keys = on;
